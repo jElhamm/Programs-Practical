9c13d4727daba1ce2e845d8f886ead78578374a4
jelhamm <elhamjafari200312@gmail.com>
2023-08-18 17:08:13 +0430

    Delete Files

--- Remove Duplicate Files/FileRemoverInterface.java
+++
@@ -1,4 +0,0 @@
-
-public interface FileRemoverInterface{
-    public void removeFiles();
-}
--- Remove Duplicate Files/FileUtilsInterface.java
+++
@@ -1,7 +0,0 @@
-
-import java.io.IOException;
-import java.util.List;
-
-public interface FileUtilsInterface {
-    List<String> getAllFiles(String inputRootPath) throws IOException;
-}
